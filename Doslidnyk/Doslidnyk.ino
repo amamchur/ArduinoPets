9fb179b47ab35e2844c2d77bc46375e3ff4138c4
Andrii Mamchur <andrii@mamchur.net>
2016-08-21 20:21:06 +0300

    Updated test app example

--- Doslidnyk/Doslidnyk.ino
+++ Doslidnyk/Doslidnyk.ino
@@ -1,117 +1,147 @@
 #include <ARDK.h>
+#include <LiquidCrystal_I2C.h>
+#include "Animation.h"
 
 using namespace ARDK;
+using namespace ARDK::IO;
 
-const int PIN_DS = 11;
-const int PIN_STCP = 12;
-const int PIN_SHCP = 13;
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
-using namespace ARDK;
-
-const IC::IC74HC595<>::Config cfg PROGMEM = {
+const IC::IC74HC595<>::Config cfg595 PROGMEM = {
   .DS = 11,
-  .STCP = 12,
-  .SHCP = 13
+  .STCP = 13,
+  .SHCP = 12
 };
+
+const uint8_t BuzzerPin = 10;
+
+const IC::IC74HC165<>::Config cfg165 PROGMEM = {
+  .PL = 8,
+  .CP = 9,
+  .Q = 7
+};
+
+typedef void (*LoopHandler)();
+typedef struct ModeHanlder {
+  LoopHandler loopHandler;
+  ButtonHandler buttonHandler;
+} ModeHanlder;
 
 const byte buttonPins[] PROGMEM = {47, 45, 43, 41, 37, 35, 33, 31, 29};
-void buttonHanlder(unsigned int button, int event);
+void ic74HC595ButtonHanlder(unsigned int button, int event);
+void nullButtonHanlder(unsigned int button, int event);
 
-IO::Buttons<sizeof(buttonPins)> bm(buttonPins, buttonHanlder);
-IC::IC74HC595<> ic74HC595(&cfg);
+void nullLoopHanlder();
+void animate();
 
-const uint32_t animationFrames[] PROGMEM = {
-  0x80000001,
-  0xC0000003,
-  0xE0000007,
-  0xF000000F,
-  0xF800001F,
-  0xFC00003F,
-  0xFE00007F,
-  0xFF0000FF,
-  0xFF8001FF,
-  0xFFC003FF,
-  0xFFE007FF,
-  0xFFF00FFF,
-  0xFFF81FFF,
-  0xFFFC3FFF,
-  0xFFFE7FFF,
-  0xFFFFFFFF,
-  0xFFFE7FFF,
-  0xFFFC3FFF,
-  0xFFF81FFF,
-  0xFFF00FFF,
-  0xFFE007FF,
-  0xFFC003FF,
-  0xFF8001FF,
-  0xFF0000FF,
-  0xFE00007F,
-  0xFC00003F,
-  0xF800001F,
-  0xF000000F,
-  0xE0000007,
-  0xC0000003,
-  0x80000001,
-  0x00000000
+uint8_t applicationMode = 0xFF;
+LoopHandler loopHandler = nullLoopHanlder;
+
+ModeHanlder modeHandlers[] = {
+  {nullLoopHanlder, nullButtonHanlder},
+  {animate, nullButtonHanlder},
+  {nullLoopHanlder, ic74HC595ButtonHanlder}
 };
+
+const size_t ModeHanldersCount = sizeof(modeHandlers) / sizeof(ModeHanlder);
+
+IO::Buttons<sizeof(buttonPins)> bm(buttonPins, nullButtonHanlder);
+IC::IC74HC595<> ic74HC595(&cfg595);
+IC::IC74HC165<> ic74HC165(&cfg165);
 
 const int framesCount = sizeof(animationFrames) / sizeof(animationFrames[0]);
 const int animationDelay = 30;
 unsigned long prevFrameTime = 0;
-int frameIndex = 0;
+int animateFrameIndex = 0;
 
 void animate() {
   unsigned long dt = millis() - prevFrameTime;
   if (dt > animationDelay) {
-    uint32_t frame = pgm_read_dword_near(animationFrames + frameIndex);
-    ic74HC595 << lsb<uint32_t>(frame) << end;
-    frameIndex = (frameIndex + 1) % framesCount;
+    uint32_t frame = pgm_read_dword_near(animationFrames + animateFrameIndex);
+    ic74HC595 << frame << end;
+    animateFrameIndex = (animateFrameIndex + 1) % framesCount;
     prevFrameTime = millis();
   }
 }
 
-void buttonHanlder(unsigned int button, int event) {
+void nullButtonHanlder(unsigned int button, int event) {
+}
+
+void ic74HC595ButtonHanlder(unsigned int button, int event) {
   if (event != IO::BUTTON_EVENT_PRESS) {
     return;
   }
 
   Serial.println(button);
 
+  uint8_t value;
+
   switch (button) {
     case 0:
-      ic74HC595 << b0 << end;
+      ic74HC595 << w0 << end;
       break;
     case 1:
-      ic74HC595 << b1 << end;
+      ic74HC595 << w1 << end;
       break;
     case 2:
-      ic74HC595 << lsb<uint32_t>(0xF000F731) << end;
+      ic74HC595 << (uint32_t)0xF000F731 << end;
       break;
     case 3:
-      ic74HC595 << msb<uint32_t>(0xF000F731) << end;
+      ic74HC595 << (uint32_t)0xF000F731 << end;
       break;
     case 4:
-      ic74HC595 << lsb<uint8_t>(0x83) << end;
+      ic74HC595 << (uint8_t)0x83 << end;
       break;
     case 5:
-      ic74HC595 << msb<uint8_t>(0x83) << end;
+      ic74HC595 << (uint8_t)0x83 << end;
       break;
     case 6:
+      ic74HC165 >> start >> value;
+      ic74HC595 << (uint32_t)value;
       break;
     case 7:
-      ic74HC595 << lsb<uint64_t>(0) << end;
+      tone(BuzzerPin, 220, 200);
       break;
   }
 }
 
+void nullLoopHanlder() {
+}
+
+void readApplicationMode() {
+  uint8_t mode;
+  ic74HC165 >> start >> mode;
+  mode = min(mode, ModeHanldersCount - 1);
+  if (applicationMode == mode) {
+    return;
+  }
+
+  tone(BuzzerPin, 440, 100);
+
+  applicationMode = mode;
+  loopHandler = modeHandlers[applicationMode].loopHandler;
+  bm.handler = modeHandlers[applicationMode].buttonHandler;
+  ic74HC595 << (uint32_t)0;
+}
+
 void setup() {
   Serial.begin(9600);
-  ARDK::LSB<int> q(3);
+
+  pinMode(BuzzerPin, OUTPUT);
+  lcd.init();
   bm.begin();
   ic74HC595.begin();
+  ic74HC165.begin();
+  readApplicationMode();
+
+  lcd.backlight();
+  lcd.setCursor(3, 0);
+  lcd.print("Hello, world!");
 }
 
 void loop() {
   bm.handle();
+  readApplicationMode();
+  loopHandler();
 }
 
