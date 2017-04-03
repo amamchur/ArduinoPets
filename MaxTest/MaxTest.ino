523bf703213f6982b2f68374e2d09c724c79c75e
Andrii Mamchur <amam@softserveinc.com>
2017-04-03 16:54:55 +0300

    Updated MAX7221 example

--- MaxTest/MaxTest.ino
+++ MaxTest/MaxTest.ino
@@ -1,5 +1,5 @@
 #include "LedControl.h"
-#include "font8x8_basic.h"
+#include "MotorPump.h"
 #include <ARDK.h>
 using namespace ARDK::IO;
 using namespace ARDK::IC;
@@ -15,11 +15,14 @@ using namespace ARDK::IC;
 */
 #define DeviceCount 4
 
+//#include "font8x8_basic.h"
+
 MAX7221<D12, D10, D11> max7221;
-MAX7221Matrix<4> matrix;
+MAX7221Data<4> matrix;
 
 LedControl lc = LedControl(12, 11, 10, DeviceCount);
-//
+int index = 0;
+
 void setup() {
   max7221.begin();
 
@@ -30,12 +33,28 @@ void setup() {
   }
 
   matrix.clear();
-  matrix.pushColumn(0xAA);
-  matrix.pushColumn(0xF0);
- 
+  //  matrix.data[0][0] = ch[0];
+  //  matrix.data[0][1] = ch[1];
+  //  matrix.data[0][2] = ch[2];
+  //  matrix.data[0][3] = ch[3];
+  //  matrix.data[0][4] = ch[4];
+  //  matrix.data[0][5] = ch[5];
+  //  matrix.data[0][6] = ch[6];
+  //  matrix.data[0][7] = ch[7];
   max7221.display(matrix);
 }
 
 void loop() {
+  uint8_t frame[8];
+  for (int i = 0; i < 8; i++) {
+    matrix.data[0][i] = pgm_read_byte_near(&IMAGES[0][0] + index * 8 + i);
+  }
+  
+//  matrix.setRows(0, frame);
+  max7221.display(matrix);
+  delay(70);
+
+  index++;
+  index %= IMAGES_LEN;
 }
 
