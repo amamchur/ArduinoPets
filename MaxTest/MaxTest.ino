03bdf55e4e12c25b16d482db08009ecdff4b4612
Andrii Mamchur <andrii@mamchur.net>
2017-04-09 17:28:02 +0300

    Updated example

--- MaxTest/MaxTest.ino
+++ MaxTest/MaxTest.ino
@@ -6,30 +6,39 @@ using namespace ARDK::IC;
 
 const uint8_t DeviceCount = 4;
 
-/*
-  pin 12 is connected to the DataIn
-  pin 11 is connected to the CLK
-  pin 10 is connected to LOAD
+/* Custom SPI configuration
+ * Exchange Clock Pin 13 to 12
+ * pin 11 -> DIN (MOSI)
+ * NAP = Not A Pin (ignore MISO)
+ * pin 12 -> CLK
 */
+//typedef CustomSPI<D11, NAP, D12> SPI;
 
-typedef CustomSPI<D12, NAP, D11> SPI;
+/*
+  Native SPI (Arduino UNO Pins) as Custom SPI
+  See https://www.arduino.cc/en/reference/SPI
+  pin 11 -> DIN (MOSI)
+  pin 12 -> not connected (MISO)
+  pin 13 -> CLK
+*/
+typedef CustomSPI<D11, D12, D13> SPI;
+
+/*
+ * Navive Arduino UNO SPI
+ */
+
+//typedef MasterAvrSPI<1> SPI;
+
+/* pin 10 is connected to LOAD (CS) */
 typedef MAX7221<SPI, D10> Device;
+
 Device max7221;
 MAX7221Data<DeviceCount> matrix;
 
 int index = 0;
 
 void setup() {
-  Serial.begin(9600);
-  matrix.clear();
-
-  max7221.begin();
-  max7221(DeviceCount, Device::TestOff);
-
-  max7221() << Device::On << Device::On << Device::On << Device::On;
-  //  max7221(DeviceCount, Device::On);
-
-  //max7221() << Device::N(DeviceCount, Device::Intencity | 0);
+  max7221.begin().init(DeviceCount);
   max7221() << (Device::Intencity | 0x0)
             << (Device::Intencity | 0x4)
             << (Device::Intencity | 0x8)
